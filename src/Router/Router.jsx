7d9a26ea68023f3dfa01804e8114b28633b974b4
Ahad Ahmed <ahadahmedcc@gmail.com>
2026-04-30 23:56:39 +0600

    basic setup ract router and tailwind css

---
+++ src/Router/Router.jsx
@@ -0,0 +1,10 @@
+
+import { createBrowserRouter } from 'react-router-dom';
+
+const Router = createBrowserRouter([
+    {
+        path:'/',
+        element:<div className='text-8xl '>Helloooo</div>
+    }
+])
+export default Router;
